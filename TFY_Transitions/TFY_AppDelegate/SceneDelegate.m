2368d4e7ac6a5c2d88b2bf7f49c1d33bda25bc79
田风有 <420144542@qq.com>
2021-01-17 20:28:15 +0800

    控制器转换动画
    控制器转换动画

---
+++ TFY_Transitions/TFY_AppDelegate/SceneDelegate.m
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.m
+//  TFY_Transitions
+//
+//  Created by 田风有 on 2021/1/17.
+//
+
+#import "SceneDelegate.h"
+#import "ViewController.h"
+@interface SceneDelegate ()
+
+@end
+
+@implementation SceneDelegate
+
+
+- (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions  API_AVAILABLE(ios(13.0)){
+    UIWindowScene *windowScene = (UIWindowScene *)scene;
+    self.window = [[UIWindow alloc] initWithWindowScene:windowScene];
+    self.window.frame = windowScene.coordinateSpace.bounds;
+    UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:[ViewController new]];
+    self.window.rootViewController = nav;
+    [self.window makeKeyAndVisible];
+}
+
+
+
+
+@end
